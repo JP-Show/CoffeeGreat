e7d7242fc05ed20929c99edd992b6be68e8e3bbf
Andre Luiz Parrine Tavares <andreluiz.jpofi@gmail.com>
2022-03-01 19:08:09 -0300

    bug menu

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 const menuNav = document.querySelector('div#menuNav')
 const menuNavOpen = document.querySelector('div#menuNavOpen')
-// menuNav.addEventListener('click', () => {
-//   menuNavOpen.classList.toggle('on', show) // "on" mais o show vai evitar de bugar de abrir na  hora errada
-//   // adicionando o menuNavOpenn e vai adicionar a lista de classes via toggle , o toggle vai adicionar e tirar
-//   show = !show
-//   // como isso ele vai atualizar o "show" ppara uum show diferente, o show false
-// })
 
 let show = true
 menuNav.addEventListener('click', menuToggle)
